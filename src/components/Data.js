8dd9a9986687599f3fed080e10ef58304f5d1e04
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-08 17:28:24 -0800

    Data.js - created

---
+++ src/components/Data.js
@@ -0,0 +1,33 @@
+export const portfolios = [
+    {
+        "name": "TravelPlanet",
+        "address": "https://vigorous-cori-21594f.netlify.app/",
+        "skills": [ "react", "redux", "mongodb", "nodejs" ],
+    },
+    {
+        "name": "PlanetBlog",
+        "address": "https://elastic-rosalind-b16ff7.netlify.app/",
+        "skills": [ "react", "redux" ],
+    }, 
+    {
+        "name": "MoviePlanet",
+        "address": "https://distracted-yonath-a65aca.netlify.app/",
+        "skills": [ "react", "axios" ],
+    },
+    {
+        "name": "NewsViewer",
+        "address": "",
+        "skills": [ "react", "axios" ],
+    },
+    {
+        "name": "Momentum",
+        "address": "https://paulsplanet.github.io/chromeapp-todolist/",
+        "skills": [ "html", "css", "javascript" ],
+    },
+    {
+        "name": "StarTalk",
+        "address": "https://fervent-northcutt-80526b.netlify.app/",
+        "skills": [ "html", "css" ],
+    },
+]
+
