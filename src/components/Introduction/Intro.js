bf610737540a2b5f93b2afc0576d98b60c2a24eb
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-09 15:56:49 -0800

    Typing Animation for Intro.js

---
+++ src/components/Introduction/Intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Typing from "./Typing";
+
+const Intro = () => {
+
+    return(
+        <Typing />
+    )
+};
+
+export default Intro;
---
+++ src/components/Introduction/Typing.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const TypeTexting = keyframes`
+    100%{
+            left: 108%;
+        }
+`;
+
+const IntroBox = styled.div`
+    width: 555px;
+    height: 220px;
+    font-size: 60px;
+    font-weight: 800;
+`;
+
+const CyonColor = styled.span`
+    color: #5AFFC4;
+`;
+
+const TypeBox = styled.span`
+    position: relative;
+    
+    &:after {
+        content: "";
+        position: absolute;
+        background-color: #1C2133;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        border-left: 3px solid white;
+        animation: ${TypeTexting} 2.5s steps(15) infinite;
+    }
+`;
+
+const Typing = () => {
+
+    return(
+        <IntroBox>
+            
+                <CyonColor>&lt;b&gt;</CyonColor>Hello<CyonColor>&lt;b&gt;</CyonColor><br />
+                <CyonColor>I'm Paul</CyonColor><br />
+                <TypeBox>Web developer</TypeBox>
+            
+        </IntroBox>
+    )
+}
+
+export default Typing;
