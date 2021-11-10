01812c17ac4afbaf322d309f851cdd028812b4ce
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-09 20:42:19 -0800

    Description - first ver. (linkedin, email)

--- src/components/Introduction/Description.js
+++ src/components/Introduction/Description.js
@@ -10,12 +10,36 @@ const AboutMe = styled.div`
    margin-bottom: 15px;
 `;
 
-const Button = styled.button`
+const ButtonLinkedin = styled.button`
     width: 178px;
     height: 48px;
     border-radius: 8px;
     font-weight: 800;
     font-size: 16px;
+
+    background-color: white;
+    color: #151928;
+    margin-right: 8px;
+
+    &:hover {
+        background-color: #EEE;
+    }
+`;
+
+const ButtonEmail = styled.button`
+    width: 178px;
+    height: 48px;
+    border-radius: 8px;
+    font-weight: 800;
+    font-size: 16px;
+
+    background-color: #1C2133;
+    color: white;
+    border: 1px solid white;
+
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
 `;
 
 const Description = () => {
@@ -28,9 +52,14 @@ const Description = () => {
                 Right now, I'm learning JS &amp; React and looking forward on creating Web Applications sometimes in the future. <br />
                 I love playing tennis with buddies. I enjoy to go to the Musemum as well.
             </AboutMe>
-            <div>
-                <Button style={{ backgroundColor: "white", color: "#151928", marginRight: "8px" }}>LinkedIn</Button>
-                <Button style={{ backgroundColor: "#1C2133", color: "white", border: "1px solid white"}}>Email me</Button>
+            <div> 
+                <a href="https://github.com/paulsplanet" target="_blank" rel="noreferrer noopner">  
+                    <ButtonLinkedin>LinkedIn</ButtonLinkedin>
+                </a>
+                
+                <a href="mailto:paulsplanet612@gmail.com">
+                    <ButtonEmail>Email me</ButtonEmail>
+                </a>     
             </div>
         </div>
     )
