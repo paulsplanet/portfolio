2ef9afcfb7310f8d9b6a8944db3216a750753f0c
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-09 20:43:31 -0800

    App.js - style set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import Intro from "./components/Introduction/Intro";
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding: 140px 0;
+`;
+
+const Frame = styled.div`
+  width: 1140px;
+  margin: 0 auto;
+`;
 
 const App = () => {
 
   return (
-    <div>
-      My Portfolio
-    </div>
+    <Background>
+      <Frame>
+        <Intro />
+      </Frame>
+    </Background>
   )
 }
 
