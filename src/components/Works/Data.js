31c00970d13bd59ebe1f3c53c72b2e34ebb23fda
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-11 15:05:03 -0800

    add content on Data

--- src/components/Works/Data.js
+++ src/components/Works/Data.js
@@ -3,31 +3,37 @@ export const portfolios = [
         "name": "TravelPlanet",
         "address": "https://vigorous-cori-21594f.netlify.app/",
         "skills": [ "react", "redux", "mongodb", "nodejs" ],
+        "content": "Login with { id: guest@gmail.com, password: 123123 }, you can find Cart, Mypick, Hisotry and etc.",
     },
     {
         "name": "PlanetBlog",
         "address": "https://elastic-rosalind-b16ff7.netlify.app/",
         "skills": [ "react", "redux" ],
+        "content": "Type your name and login with any password. You can see your name on top and NewPost button in Orange.",
     }, 
     {
         "name": "MoviePlanet",
         "address": "https://distracted-yonath-a65aca.netlify.app/",
         "skills": [ "react", "axios" ],
+        "content": "Please enjoy to find Movie and TV program information here.",
     },
     {
         "name": "NewsViewer",
         "address": "https://paulsplanet.github.io/newsviewer_introduction/",
         "skills": [ "react", "axios" ],
+        "content": "This is just NewsViewer presentation becuase of API license.",
     },
     {
         "name": "Momentum",
         "address": "https://paulsplanet.github.io/chromeapp-todolist/",
         "skills": [ "html", "css", "javascript" ],
+        "content": "There is weather, time, and Todo list. Please set this your first page.",
     },
     {
         "name": "StarTalk",
         "address": "https://paulsplanet.github.io/startalk/",
         "skills": [ "html", "css" ],
+        "content": "This is cloning Kakao Talk. Hope you enjoy this.",
     },
 ]
 
