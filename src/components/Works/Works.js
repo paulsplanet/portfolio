3982f6ef659305916f4a8a98ba4f9047069ee602
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-11 12:02:09 -0800

    works display - first draft

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,11 +1,43 @@
 import React from "react";
+import { portfolios} from "./Data";
+import styled from "styled-components";
+import Momentum from "./assets/Momentum.JPG";
+import MoviePlanet from "./assets/MoviePlanet.JPG";
+import NewsViewer from "./assets/NewsViewer.JPG";
+import PlanetBlog from "./assets/PlanetBlog.JPG";
+import StarTalk from "./assets/StarTalk.JPG";
+import TravelPlanet from "./assets/TravelPlanet.JPG";
+
+const PortfolioWrapper = styled.div`
+    width: 360px;
+    height: 370px;
+
+    .TravelPlanet{ background-image: url(${TravelPlanet}) }
+    .Momentum{ background-image: url(${Momentum}) }
+    .MoviePlanet{ background-image: url(${MoviePlanet}) }
+    .NewsViewer{ background-image: url(${NewsViewer}) }
+    .PlanetBlog{ background-image: url(${PlanetBlog}) }
+    .StarTalk{ background-image: url(${StarTalk}) }
+    
+`;
+
+const Thumbnail = styled.div`
+    width: 360px;
+    height: 290px;
+    background-size: cover;
+    
+`;
+
 
 const Works = () => {
 
     return (
-        <div>
-
-        </div>
+       portfolios.map((portfolio, index) => (
+           <PortfolioWrapper id={index}>
+               <a href={portfolio.address}><Thumbnail className={portfolio.name} /></a>
+               <span>{portfolio.name}</span>
+           </PortfolioWrapper>
+       ))
     )
 }
 
