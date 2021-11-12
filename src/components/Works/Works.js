1ad26c7eb39e2a3d6fb3b34e12852f542248b244
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-11 19:18:16 -0800

    skills icon hover function

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -8,11 +8,20 @@ import PlanetBlog from "./assets/PlanetBlog.JPG";
 import StarTalk from "./assets/StarTalk.JPG";
 import TravelPlanet from "./assets/TravelPlanet.JPG";
 
+import react from "../Icons/react.svg";
+import axios from "../Icons/axios.svg";
+import css from "../Icons/css.svg";
+import html from "../Icons/html.svg";
+import javascript from "../Icons/javascript.svg";
+import mongodb from "../Icons/mongodb.svg";
+import nodejs from "../Icons/nodejs.svg";
+import redux from "../Icons/redux.svg";
+
 const PortfolioWrapper = styled.div`
     width: 360px;
     height: 440px;
     position: relative;
-
+    
     .TravelPlanet{ background-image: url(${TravelPlanet}) }
     .Momentum{ background-image: url(${Momentum}) }
     .MoviePlanet{ background-image: url(${MoviePlanet}) }
@@ -22,12 +31,53 @@ const PortfolioWrapper = styled.div`
     
 `;
 
+const HoverWrapper = styled.div`
+    position: absolute;
+    top: 0px;
+    width: 360px;
+    height: 290px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .react { background-image: url(${react}) }
+    .axios { background-image: url(${axios}); width: 90px }
+    .css { background-image: url(${css}) }
+    .html { background-image: url(${html}) }
+    .javascript { background-image: url(${javascript}) }
+    .mongodb { background-image: url(${mongodb}) }
+    .nodejs { background-image: url(${nodejs}); width: 90px }
+    .redux { background-image: url(${redux}) }
+`;
+
 const SkillsHover = styled.div`
     position: absolute;
     top: 0px;
     width: 360px;
     height: 290px;
     background-color: #1C2133;
+    opacity: 0;
+    transition: opacity 0.2s linear;
+    z-index: 1;
+`;
+
+const SkillsWrapper = styled.div`
+    display: flex;
+    width: 180px;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+`;
+
+const Skill = styled.div`
+    height: 50px;
+    width: 50px;
+    margin: 0 5px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: relative;
+    z-index: 10;
     opacity: 0;
     transition: opacity 0.2s linear;
 `;
@@ -53,29 +103,40 @@ const Content = styled.div`
 const Anchor = styled.a`
    &:hover {
         ${SkillsHover} {
-            opacity: 0.6;
+            opacity: 0.5;
         }; 
         ${Thumbnail} {
-            opacity: 0.6;
+            opacity: 0.5;
+        };
+        ${Skill} {
+            opacity: 1;
         }
     } 
 `;
 
 
-
 const Works = () => {
 
     return (
+       
        portfolios.map((portfolio, index) => (
            <PortfolioWrapper id={index}>
                <Anchor href={portfolio.address} target="_blank" rel="noreferrer noopener">
                    <Thumbnail className={portfolio.name} />
-                   <SkillsHover>AAA</SkillsHover>
+                   <SkillsHover />
+                   <HoverWrapper>
+                        <SkillsWrapper>
+                            {portfolio.skills.map(skill => 
+                                    (<Skill className={skill} />) 
+                                )}
+                        </SkillsWrapper>
+                    </HoverWrapper>
                </Anchor>
                <WorkTitle>{portfolio.name}</WorkTitle>
                <Content>{portfolio.content}</Content>
            </PortfolioWrapper>
        ))
+
     )
 }
 
